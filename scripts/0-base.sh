960306b86592ccd14f879eeb04ff2ee89c66693c
hhhhhhhhhn <joseugarte2262005@gmail.com>
2021-02-06 20:22:04 -0300

    started base install

--- scripts/0-base.sh
+++ scripts/0-base.sh
@@ -1,9 +1,18 @@
-#: Drive
-echo "DRIVE: $DRIVE"
-sleep 5
+#!/bin/sh
+#: System Clock
+timedatectl set-ntp true
 
-#: Done
-sleep 5
+#: Partitions
+dd if=/dev/zero "of=/dev/$DRIVE" bs=1024 count=1 conv=notrunc
 
-#: Last
-sleep 12
+gdisk "/dev/$DRIVE" < assets/gdisk.txt
+
+mkfs.fat -F32 "/dev/${DRIVE}2"
+mkfs.ext4 "/dev/${DRIVE}3"
+
+mount "/dev/${DRIVE}3" /mnt
+mkdir -p /mnt/boot/EFI
+mount "/dev/${DRIVE}2" /mnt/boot/EFI
+
+#: Base system
+pacstrap /mnt base linux linux-firmware
