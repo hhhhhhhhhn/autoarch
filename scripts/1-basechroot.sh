ca0ef683d7dda6d048a52a501c0a04b6142ceb14
hhhhhhhhhn <joseugarte2262005@gmail.com>
2021-02-07 10:26:36 -0300

    added locale-gen command

--- scripts/1-basechroot.sh
+++ scripts/1-basechroot.sh
@@ -5,6 +5,7 @@ hwclock --systohc
 
 mv /etc/locale.gen /etc/locale.gen.old
 echo "$LOCALES" | tr "|" "\n" > /etc/locale.gen
+locale-gen
 echo "LANG=$(tail -n 1 < /etc/locale.gen | sed "s/\s*$//")" > /etc/locale.conf
 echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
 echo Arch > /etc/hostname
