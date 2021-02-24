beb5581cfc5698260a32cf598afdbdc85505dd5c
hhhhhhhhhn <joseugarte2262005@gmail.com>
2021-02-23 22:17:46 -0300

    moved grub install to chroot

--- scripts/1-basechroot.sh
+++ scripts/1-basechroot.sh
@@ -16,4 +16,11 @@ yes "$ROOTPASSWORD" | passwd
 useradd -m -G wheel "$USERNAME"
 yes "$USERPASSWORD" | passwd "$USERNAME"
 
+#: Bootloader
+grub-install "--target=$(uname -m | grep 64 >/dev/null && echo x86_64 || echo i386)-efi" --recheck --removable --efi-directory=/boot/EFI --boot-directory=/boot
+
+grub-install --target=i386-pc --recheck --boot-directory=/boot "/dev/$DRIVE"
+
+grub-install --target=i386-pc --recheck --boot-directory=/boot "/dev/${DRIVE}3"
+
 grub-mkconfig -o /boot/grub/grub.cfg
